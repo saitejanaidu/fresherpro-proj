ddbf5aae17c6c2a64e49bde9fe570540565be170
Lenovo <Lenovo@Lenovo-pc>
2020-04-05 11:37:12 +0530

    comit

--- demo-1/src/main/java/com/example/demo/Demo1Application.java
+++ demo-1/src/main/java/com/example/demo/Demo1Application.java
@@ -2,11 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@Controller
 public class Demo1Application {
+	@RequestMapping("/")
+	public ModelAndView home () {
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("login");
+		return mv;
+	}
 
 	public static void main(String[] args) {
+		
+		
+		
+		
 		SpringApplication.run(Demo1Application.class, args);
 	}
 
